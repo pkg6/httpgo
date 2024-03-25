863588f25a1f2185c56f5767207c3447e005fc05
zzqqw <zhiqiangwang@aliyun.com>
2024-03-25 18:22:07 +0800

    feat: TimeCalendar

--- t/time.go
+++ t/time.go
@@ -12,3 +12,12 @@ func TimeBetween(start, end time.Time, diff time.Duration) bool {
 	}
 	return sub <= diff
 }
+
+// TimeCalendar  根据开始时间和结束时间生成期间每天的开始时间和结束时间
+func TimeCalendar(start, end time.Time, dayCalendarCallback func(day string, start, end time.Time)) {
+	for d := start; d.Unix() <= end.Unix(); d = d.AddDate(0, 0, 1) {
+		dailyStart := time.Date(d.Year(), d.Month(), d.Day(), 0, 0, 0, 0, d.Location())
+		dailyEnd := dailyStart.Add(24*60*60 - 1*time.Second)
+		dayCalendarCallback(d.Format("2006-01-02"), dailyStart, dailyEnd)
+	}
+}
